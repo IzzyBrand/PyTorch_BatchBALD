93d292c221cd1555bb7287e418bcc52fb3ec4238
Izzy Brand <izzybrand@gmail.com>
2020-08-27 14:39:19 -0700

    remove conv layers from model for simplicity

--- main.py
+++ main.py
@@ -2,8 +2,6 @@
 Massachusetts Institute of Technology
 
 Izzy Brand, 2020
-
-Model adapted from https://github.com/pytorch/examples/blob/master/mnist/main.py
 """
 import torch
 import torch.nn as nn
@@ -16,26 +14,23 @@ from torch.optim.lr_scheduler import StepLR
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, 3, 1)
-        self.conv2 = nn.Conv2d(32, 64, 3, 1)
-        self.dropout1 = nn.Dropout2d(0.25)
-        self.dropout2 = nn.Dropout2d(0.5)
-        self.fc1 = nn.Linear(9216, 128)
-        self.fc2 = nn.Linear(128, 10)
+
+        self.layers = nn.Sequential(
+            nn.Linear(784, 2048),
+            nn.Dropout(0.1),
+            nn.ReLU(),
+            nn.Linear(2048, 128),
+            nn.Dropout(0.1),
+            nn.ReLU(),
+            nn.Linear(128, 10),
+            nn.Dropout(0.1),
+            nn.ReLU(),
+        )
 
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = F.relu(x)
-        x = self.conv2(x)
-        x = F.relu(x)
-        x = F.max_pool2d(x, 2)
-        x = self.dropout1(x)
         x = torch.flatten(x, 1)
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.dropout2(x)
-        x = self.fc2(x)
+        x = self.layers(x)
         output = F.log_softmax(x, dim=1)
         return output
 
