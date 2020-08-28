6edfee55e2f26ada8f122407d9e5baed009b6972
Izzy Brand <izzybrand@gmail.com>
2020-08-28 09:13:04 -0700

    implement active learning training loop

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
 
+from aquisition import *
+
 
 class Net(nn.Module):
     def __init__(self):
@@ -50,6 +52,23 @@ def train(model, device, train_loader, optimizer, epoch):
                 100. * batch_idx / len(train_loader), loss.item()))
 
 
+def active(model, aquirer, device, optimizer, epoch, num_batches=1000):
+    model.train()
+    for batch_idx in range(num_batches):
+        data, target = aquirer.select(model)
+        #data, target = data.to(device), target.to(device)
+        # the aquirer returned a single x, so we need make it into size-1 batch
+        data, target = data[None,...].to(device), torch.LongTensor([target]).to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % 10 == 0:
+            print('Active Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), num_batches,
+                100. * batch_idx / num_batches, loss.item()))
+
 def test(model, device, test_loader):
     model.eval()
     test_loss = 0
@@ -69,7 +88,11 @@ def test(model, device, test_loader):
         100. * correct / len(test_loader.dataset)))
 
 
-def main():
+if __name__ == '__main__':
+    num_pretrain = 1000
+    num_pool = 1000
+    num_extra = 60000 - num_pretrain - num_pool
+
     # set up the GPU if one exists
     use_cuda = torch.cuda.is_available()
     device = torch.device("cuda" if use_cuda else "cpu")
@@ -79,11 +102,13 @@ def main():
         transforms.ToTensor(),
         transforms.Normalize((0.1307,), (0.3081,))
         ])
-    train_data = datasets.MNIST('data', train=True, download=True,
+    dataset = datasets.MNIST('data', train=True, download=True,
                        transform=transform)
+    pretrain_data, pool_data, _ =torch.utils.data.random_split(
+        dataset, [num_pretrain, num_pool, num_extra])
     test_data = datasets.MNIST('data', train=False,
                        transform=transform)
-    train_loader = torch.utils.data.DataLoader(train_data,
+    pretrain_loader = torch.utils.data.DataLoader(pretrain_data,
         batch_size=64, pin_memory=True, shuffle=True)
     test_loader = torch.utils.data.DataLoader(test_data,
         batch_size=64, pin_memory=True, shuffle=True)
@@ -94,14 +119,18 @@ def main():
     scheduler = StepLR(optimizer, step_size=1, gamma=0.7)
 
     # train the model and test after each epoch
-    for epoch in range(1, 4):
-        train(model, device, train_loader, optimizer, epoch)
+    for epoch in range(1, 2):
+        train(model, device, pretrain_loader, optimizer, epoch)
         test(model, device, test_loader)
         scheduler.step()
 
+    aquirer = BALD(pool_data)
+    optimizer = optim.Adadelta(model.parameters(), lr=1.0)
+
+    # train the model and test after each epoch
+    for epoch in range(1, 2):
+        active(model, aquirer, device, optimizer, epoch)
+
+
     # save model parameters
     # torch.save(model.state_dict(), "mnist_cnn.pt")
-
-
-if __name__ == '__main__':
-    main()
